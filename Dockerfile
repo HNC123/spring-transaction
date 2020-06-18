6eb91084aad2c3abce3b09471ae8467738694520
Tarang2015 <tribhuwan.sams@gmail.com>
2020-06-18 17:44:58 +0530

    new chnages

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
-FROM openjdk:8-jre
+FROM openjdk:8
+EXPOSE 8080
 COPY target/spring-transaction.jar spring-transaction.jar
 ENTRYPOINT ["java", "-jar", "/spring-transaction.jar"]
--- src/main/java/com/test/transaction/springtransaction/controller/HelloWorld.java
+++ src/main/java/com/test/transaction/springtransaction/controller/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.test.transaction.springtransaction.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +9,12 @@ public class HelloWorld {
 
   @GetMapping("/rest/docker/hello")
   public String getMessage(){
+    return "Hello Docker Container";
+
+  }
+
+  @PostMapping("/rest/docker/message")
+  public String sendMessage(){
     return "Hello Docker Container";
 
   }
