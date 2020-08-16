40172c3a2b7a95020b14712d8e0586325194c837
Tarang2015 <45654414+Tarang2015@users.noreply.github.com>
2020-08-16 16:11:43 +0530

    Update SpringTransactionApplication.java

--- src/main/java/com/test/transaction/springtransaction/SpringTransactionApplication.java
+++ src/main/java/com/test/transaction/springtransaction/SpringTransactionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringTransactionApplication {
 
   public static void main(String[] args) {
+    System.out.println("spring boot");
     SpringApplication.run(SpringTransactionApplication.class, args);
   }
 
