aa74ce0b01daa20597dec969e54c24d4f6e44fa6
Tarang2015 <tribhuwan.sams@gmail.com>
2020-08-19 10:44:12 +0530

    removed syso

--- src/main/java/com/test/transaction/springtransaction/controller/HelloWorld.java
+++ src/main/java/com/test/transaction/springtransaction/controller/HelloWorld.java
@@ -9,27 +9,18 @@ public class HelloWorld {
 
   @GetMapping("/rest/docker/hello")
   public String getMessage(){
-  System.out.println("getmessage")
-    System.out.println("docker getmessage");
     return "Hello Docker Container";
 
   }
 
   @PostMapping("/rest/docker/message")
   public String sendMessage(){
-  System.out.println("sendmessage")
-    System.out.println("sendmessage")
     return "Hello Docker Container";
 
   }
 
   @GetMapping("/rest/docker")
   public String test(){
-System.out.println("sendmessage")
-System.out.println("testing using git gui....")
-  System.out.println("testing using git gui")
-System.out.println("testing using git gui3")
-  System.out.println("testing using git gui")
     return "Hello Docker Container";
   }
 }
