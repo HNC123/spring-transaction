009e71bcf084a6f50e63ee58e99e303c684dd4e2
Tarang2015 <tribhuwan.sams@gmail.com>
2020-08-23 16:18:52 +0530

    condition

--- src/main/java/com/test/transaction/springtransaction/SpringTransactionApplication.java
+++ src/main/java/com/test/transaction/springtransaction/SpringTransactionApplication.java
@@ -7,11 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringTransactionApplication {
 
   public static void main(String[] args) {
+if(){
+return;
+}
     if(){
     System.out.pritn("dfhkdhf344k");
     }else{
 System.out.pritn("dfhkdhfk");
     }
+
+System.out.pritn("dfhkdhfk")
+if(){
+return;
+}
+
     SpringApplication.run(SpringTransactionApplication.class, args);
  
  }
